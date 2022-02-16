74b815a77b09fb0f65da3ac69b8aad2c5ec62a67
kalingobiz <kaleab.kalingolive@gmail.com>
2022-02-16 16:31:44 +0300

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,14 @@
+#  this is just  a sample config file.You need to edit this file.You need to get these details from your Twitter app settings.
+
+consumer_key = "KWs0cXoYqX2tuTwDv9EPAELvV"
+consumer_secret = "XZNNRYxceNsBhu76zENYuXy744FSGnNx8nOoaPcsO03GrZmxIZ"
+access_token = "1441749230666739713-kvj96T3nhl6p69kovVRFV9bmHWaTUq"
+access_token_secret = "DjFRguWOse0WRob8ZjFZCqyGqh9QKlseO4TWUGQGxs2BU"
+
+consumer_key2 = "EhFdo5e3REjGjLXMbh2FiDoiW"
+consumer_secret2 = "5kw1mU3Q8QGbHXCW2tM7yorhH3NEHNdoAOQunQtRd5b9dQ22V2"
+
+consumer_key7='VdhP4lrW1s6lj00Bg1gkTOzCM'
+consumer_secret7='FAy2lZYmITsObBxgl4gJ9QrlEtxX9jP5KrCnMrKgbGsObLfOFt'
+access_token7 = "1428032855901360130-bVbxr83LNASr5MrKD3tftYt0vr2MUK"
+access_token_secret7 = "AfzmFOBM0FBncvto5Q2hGQUFbwrA7TsBL2l44Cxm1iAbg"
