88df188b50cd9bdc9207434c1cf62d5f23d4b1f8
kalingobiz <kaleab.kalingolive@gmail.com>
2022-03-09 22:57:21 +0300

    Update server3.py

--- server3.py
+++ server3.py
@@ -5,7 +5,7 @@ import pickle
 ##import shrink
 import os
 import numpy as np
-import cv2
+
 from os import listdir
 from sklearn.preprocessing import LabelBinarizer
 from keras.models import Sequential
@@ -31,7 +31,7 @@ def upload_file():
 d= np.empty([5,5],dtype=str)
 __x__="jhjh "
 import pandas as pd
-import cv2 as cv
+
 import basic
 import numpy as np
 import matplotlib.pyplot as plt
@@ -44,9 +44,9 @@ from sklearn.preprocessing import scale
 files="ghjg"
 #size2=basic.size
 default_image_size = tuple((256, 256))
-model = load_model('H:/ggg.h5')
+model = load_model('ggg.h5')
 
-label_encoder = open(r'H:/label_transform.pkl', 'rb')
+label_encoder = open(r'label_transform.pkl', 'rb')
 label_transformer = pickle.load(label_encoder)
 
 def convert_image_to_array(image_dir):
@@ -72,58 +72,7 @@ def prediction(imgpath="1547811666136.jpg"):
     ret = result.split('_')
     return " ".join(ret)
 
-def shrink(x):
-    image=basic.adjustim3(x)
-    image = image.take(range(0, basic.finalsizex), axis=0).take(range(0, basic.finalsizey), axis=1)
-    # image = cv.resize(image, (32, 32), interpolation=cv.INTER_CUBIC)
-    #image = np.double(np.round((1.2 * np.log(image))))
-    s = np.empty([basic.size, 2], dtype=int)
-    s2 = np.empty([basic.size, 2], dtype=int)
-    c = 0
-    c2 = 0
-    o = np.zeros((60, 80), dtype=int)
-    o2 = np.zeros((60, 80), dtype=int)
-    '''for a in range(image.shape[0]):
-        for b in range(image.shape[1]):
-            if image[a][b] == 1:
-                image[a][b] = 1
-            else:
-                image[a][b] = 0'''
-    k = 0
-    k2 = 0
-    for r2 in range(image.shape[1]):
-        #print(image[r])
-        if np.max(image[:,r2]) == 0:
-            
-            c2 = c2 + 1
-        else:
-            s2[k2][0] = r2
-            k2 = k2 + 1
-            c2 = 0
-    #print(s2[0][0])    
-    for r in range(image.shape[0]):
-        #print(image[r])
-        if np.max(image[r]) == 0:
-            
-            c = c + 1
-        else:
-            s[k][0] = r
-            k = k + 1
-            c = 0
-     
-    #print("first",s2[0][0])
-    image2 = image.take(range(s[0][0], basic.finalsizex - c), axis=0).take(range(s2[0][0], basic.finalsizey-c2), axis=1)
-    # print(60-image2.shape[0])
-    #o = np.append(image2, np.int8(np.zeros((basic.size - image2.shape[0]) * basic.size))).reshape(basic.size, basic.size)
-    # v = v
-    # o = v
-    image2 = cv.resize(image2, (basic.finalsizex, basic.finalsizey), interpolation=cv.INTER_NEAREST)
-    # o = cv.dilate(o, kernel, iterations=1)
-    # o = cv.erode(o, kernel, iterations=1)
-    #o = o.take(range(0, basic.finalsizex), axis=0).take(range(30, 120), axis=1)
-    #o = cv.resize(o, (16, 16), interpolation=cv.INTER_CUBIC)
 
-    return image2
 
 
 @app.route('/uploader', methods = ['GET', 'POST'])
