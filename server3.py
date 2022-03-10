ba8aeb8e6858f58f1ec67dd461dc6fc9cf9e401b
kalingobiz <kaleab.kalingolive@gmail.com>
2022-03-10 16:56:40 +0300

    Update server3.py

--- server3.py
+++ server3.py
@@ -1,52 +1,31 @@
-from flask import Flask, render_template, request, redirect
-from werkzeug.utils import secure_filename
-import pandas as pd
-import pickle
-##import shrink
-import os
-import numpy as np
+import streamlit as st
+import streamlit.components.v1 as stc
 
+# File Processing Pkgs
+import pandas as pd
+import docx2txt
+from PIL import Image 
+from PyPDF2 import PdfFileReader
+import pdfplumber
 from os import listdir
+import os
 from sklearn.preprocessing import LabelBinarizer
 from keras.models import Sequential
 ##from keras.layers.normalization import BatchNormalization
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-
-
-
-app = Flask(__name__)
-
-app.config['APPLICATION_ROOT']="upload/"
-#shrink.files=0
-FOLDER = os.path.join('static','test')
-app.config['UPLOAD_FOLDER']=FOLDER
-@app.route('/')
-def upload_file():
-   #s=pd.read_html("upload.html")
-   #s=open("applications.html","rb")
-   #list_templates()
-   return render_template("upload.html")
-	
-d= np.empty([5,5],dtype=str)
-__x__="jhjh "
 import pandas as pd
-
-
+import cv2 
+import pickle
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import scale
-#data = pd.read_csv("list.csv")
-#y = np.array(data)
-
-files="ghjg"
-#size2=basic.size
 default_image_size = tuple((256, 256))
-model = load_model('ggg.h5')
+model = load_model('C:/Users/kal/Documents/ttiter/ggg.h5')
 
-label_encoder = open(r'label_transform.pkl', 'rb')
+label_encoder = open(r'H:/label_transform.pkl', 'rb')
 label_transformer = pickle.load(label_encoder)
 
 def convert_image_to_array(image_dir):
@@ -71,47 +50,114 @@ def prediction(imgpath="1547811666136.jpg"):
     result = label_transformer.inverse_transform(pred)[0].replace('\n','')
     ret = result.split('_')
     return " ".join(ret)
+def read_pdf(file):
+	pdfReader = PdfFileReader(file)
+	count = pdfReader.numPages
+	all_page_text = ""
+	for i in range(count):
+		page = pdfReader.getPage(i)
+		all_page_text += page.extractText()
+
+	return all_page_text
+
+def read_pdf_with_pdfplumber(file):
+	with pdfplumber.open(file) as pdf:
+	    page = pdf.pages[0]
+	    return page.extract_text()
+
+# import fitz  # this is pymupdf
+
+# def read_pdf_with_fitz(file):
+# 	with fitz.open(file) as doc:
+# 		text = ""
+# 		for page in doc:
+# 			text += page.getText()
+# 		return text 
+
+# Fxn
+@st.cache
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img 
 
 
 
+def main():
+	st.title("File Upload Tutorial")
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_files():
+	menu = ["Home","Dataset","DocumentFiles","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
 
-    if request.method == 'POST':
-          f = request.files['file']
-          print (f)
-##          s = request.form['class']
-          #print(s)
-          #print(f)
-          f.save("static/test/"+secure_filename(f.filename))
-          he= open("head.txt")
-          head = he.read()
-          fo= open("foot.txt")
-          foot = fo.read()
-          pre= open("preview.txt")
-          preview = pre.read()
-          pickle_in = open("svm.pickle","rb")
-          
-          x="static/test/"+secure_filename(f.filename)
-##          image= shrink(x)
-          predicted=[prediction(x)]
-##          tests2 = image.reshape(1,-1)
-##          shrink.files=np.str(x)
-##          classifier = pickle.load(pickle_in)
-##          predicted = classifier.predict(tests2)
-##          print(predicted)
-          
-                
-    
-	
-
-      
-      
-    uimage = os.path.join(app.config['UPLOAD_FOLDER'],request.files['file'].filename)     
-    return render_template("heads.html",user_image=uimage, predict=predicted[0])
-      
-  	
+	if choice == "Home":
+		st.subheader("Home")
+		image_file = st.file_uploader("Upload Image",type=['png','jpeg','jpg'])
+		if image_file is not None:
 		
+			# To See Details
+			# st.write(type(image_file))
+			# st.write(dir(image_file))
+			file_details = {"Filename":image_file.name,"FileType":image_file.type,"FileSize":image_file.size}
+			st.write(file_details)
+
+			img = load_image(image_file)
+			st.image(img,width=250)
+			with open(os.path.join("static",image_file.name),"wb") as f:
+			  	f.write((image_file).getbuffer())
+			predicted=prediction("static/"+image_file.name)
+			st.text("Done")
+			st.write(predicted)
+
+
+	elif choice == "Dataset":
+		st.subheader("Dataset")
+		data_file = st.file_uploader("Upload CSV",type=['csv'])
+		if st.button("Process"):
+			if data_file is not None:
+				file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+				st.write(file_details)
+
+				df = pd.read_csv(data_file)
+				st.dataframe(df)
+
+	elif choice == "DocumentFiles":
+		st.subheader("DocumentFiles")
+		docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
+		if st.button("Process"):
+			if docx_file is not None:
+				file_details = {"Filename":docx_file.name,"FileType":docx_file.type,"FileSize":docx_file.size}
+				st.write(file_details)
+				# Check File Type
+				if docx_file.type == "text/plain":
+					# raw_text = docx_file.read() # read as bytes
+					# st.write(raw_text)
+					# st.text(raw_text) # fails
+					st.text(str(docx_file.read(),"utf-8")) # empty
+					raw_text = str(docx_file.read(),"utf-8") # works with st.text and st.write,used for futher processing
+					# st.text(raw_text) # Works
+					st.write(raw_text) # works
+				elif docx_file.type == "application/pdf":
+					# raw_text = read_pdf(docx_file)
+					# st.write(raw_text)
+					try:
+						with pdfplumber.open(docx_file) as pdf:
+						    page = pdf.pages[0]
+						    st.write(page.extract_text())
+					except:
+						st.write("None")
+					    
+					
+				elif docx_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+				# Use the right file processor ( Docx,Docx2Text,etc)
+					raw_text = docx2txt.process(docx_file) # Parse in the uploadFile Class directory
+					st.write(raw_text)
+
+	else:
+		st.subheader("About")
+		st.info("Built with Streamlit")
+		st.info("Jesus Saves @JCharisTech")
+		st.text("Jesse E.Agbe(JCharis)")
+
+
+
 if __name__ == '__main__':
-   app.run(host="localhost", port=5002, debug=False)
+	main()
